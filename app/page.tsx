5fc7b76e531dd6a66fcd41b329942892aa4e1f46
Tengu712 <genreihoutengu@gmail.com>
2023-08-02 17:14:05 +0900

    fix deadline

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <h1 className="center">東方明理杯・参 エントリーフォーム</h1>
         <p>参加希望の方は必ず本フォームに回答してください。</p>
         <p>連絡先にDiscordアカウントを使用します。アカウントを作成していない方は、<a href="https://support.discord.com/hc/ja/articles/360033931551-の始め方#:~:text=ご覧ください。-,アカウントを作成する,-Discordの登録">こちら</a>を参考に作成してから本フォームへ回答してください。</p>
-        <p>締切は8/12（土） 20:00です。</p>
+        <p>締切は8/10（木） 20:00です。</p>
         <p className="center fs-1_5rem"><a href="./form">エントリーフォームに進む</a></p>
       </div>
     </main>
