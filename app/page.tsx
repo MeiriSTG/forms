4d069e20aefab4c76bb3ed9ba08258002267ed01
Tengu712 <genreihoutengu@gmail.com>
2023-08-11 00:28:51 +0900

    close form

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ export default function Home() {
     <main>
       <div>
         <h1 className="center">東方明理杯・参 エントリーフォーム</h1>
-        <p>参加希望の方は必ず本フォームに回答してください。</p>
-        <p>連絡先にDiscordアカウントを使用します。アカウントを作成していない方は、<a href="https://support.discord.com/hc/ja/articles/360033931551-の始め方#:~:text=ご覧ください。-,アカウントを作成する,-Discordの登録">こちら</a>を参考に作成してから本フォームへ回答してください。</p>
-        <p>締切は8/10（木） 20:00です。</p>
-        <p className="center fs-1_5rem"><a href="./form">エントリーフォームに進む</a></p>
+        <p>エントリーは終了しました。</p>
       </div>
     </main>
   )
