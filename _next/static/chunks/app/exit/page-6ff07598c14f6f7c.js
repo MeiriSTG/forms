bc36b6f7fd46b9c620a59d83c5bc182e1b264f02
Tengu712 <genreihoutengu@gmail.com>
2023-08-11 00:34:18 +0900

    close form

---
+++ _next/static/chunks/app/exit/page-6ff07598c14f6f7c.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[976],{15:function(e,r,n){Promise.resolve().then(n.bind(n,247))},247:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return i}});var s=n(7437),t=n(4033);function i(){let e=(0,t.useSearchParams)(),r=e.get("url");return r?(0,s.jsx)("main",{children:(0,s.jsxs)("div",{children:[(0,s.jsx)("p",{children:"エントリーを受け付けました。"}),(0,s.jsx)("p",{children:"エントリー内容は、設定したIDとPasswordを用いて、エントリーフォームから修正できます。"}),(0,s.jsx)("p",{children:(0,s.jsx)("a",{href:r,children:"ここから運営サーバーに参加して下さい！"})}),(0,s.jsx)("hr",{className:"spacer"}),(0,s.jsx)("p",{className:"right",children:(0,s.jsx)("a",{href:"/forms/",children:"トップページに戻る"})})]})}):(0,s.jsx)("main",{children:(0,s.jsx)("div",{children:(0,s.jsx)("p",{children:"不正なアクセスです。"})})})}},622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var s=n(2265),t=Symbol.for("react.element"),i=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,o=s.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,l={key:!0,ref:!0,__self:!0,__source:!0};function f(e,r,n){var s,i={},f=null,u=null;for(s in void 0!==n&&(f=""+n),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(u=r.ref),r)c.call(r,s)&&!l.hasOwnProperty(s)&&(i[s]=r[s]);if(e&&e.defaultProps)for(s in r=e.defaultProps)void 0===i[s]&&(i[s]=r[s]);return{$$typeof:t,type:e,key:f,ref:u,props:i,_owner:o.current}}r.Fragment=i,r.jsx=f,r.jsxs=f},7437:function(e,r,n){"use strict";e.exports=n(622)},4033:function(e,r,n){e.exports=n(8165)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=15)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/layout-67c791e6808ced3e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9644:function(n,e,u){Promise.resolve().then(u.t.bind(u,3516,23))},3516:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=9644)}),_N_E=n.O()}]);
